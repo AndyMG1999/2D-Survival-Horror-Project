4753f52af6b7002d0f032994c2eb3bb3b8e70bbc
Andy Gonzalez <gonzalezandy21@gmail.com>
2025-05-10 21:20:43 -0400

    Added Player Inventory and Item Class
    - Added Player Inventory Controller
    - Added Item Class
    - Added on to Gitignore

---
+++ Assets/Scripts/Player/Item.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public enum ItemTypes {
+        Key_Item,
+        Weapon,
+        Ammo,
+        Consummable,
+    };
+    
+[System.Serializable]    
+public class Item
+{
+    public Sprite itemIcon;
+    public ItemTypes itemType;
+    public string itemName="";
+    public string itemDescription="";
+    public int itemAmount=1;
+    public GameObject gameObject;
+}
